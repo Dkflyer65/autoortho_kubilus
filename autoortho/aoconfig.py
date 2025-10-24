58e69fc61b2cdd5d8cfbac00b6aad2b4d39cdba8
dkflyer65 <allan@m36.dk>
2025-10-24 18:47:53 +0200

    Ignore macos .DS_Store
    (cherry picked from commit 77aa318d9bc36fedf4c5d1d1395456dbc19ab179)

--- autoortho/aoconfig.py
+++ autoortho/aoconfig.py
@@ -151,6 +151,7 @@ prefer_winfsp = False
         sceneries = []
         if os.path.exists(self.ao_scenery_path):
             sceneries = os.listdir(self.ao_scenery_path)
+            sceneries = [s for s in sceneries if s != '.DS_Store']
             print(f"Found sceneries: {sceneries}")
 
         self.scenery_mounts = [{
